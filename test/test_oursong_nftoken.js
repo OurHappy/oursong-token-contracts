5942a30858d05983ede8c088d88fdd93b8e58985
Fukuball Lin <fukuball@gmail.com>
2021-03-16 13:59:49 +0800

    Add test

--- test/test_oursong_nftoken.js
+++ test/test_oursong_nftoken.js
@@ -12,6 +12,7 @@ contract('OurSongNFToken', function (accounts) {
     const TOKEN_NAME = 'OurSongNFToken';
     const TOKEN_SYMBOL = 'OSNFT';
     const INITIAL_SUPPLY = new BN(5);
+    const ADD_SUPPLY = new BN(2);
     const INITIAL_BASE_URI = 'https://www.oursong.com/project/custom_id/token/';
     const NEW_BASE_URI = 'https://new.oursong.com/project/custom_id/token/';
 
@@ -56,6 +57,78 @@ contract('OurSongNFToken', function (accounts) {
         "outputs": [],
         "stateMutability": "nonpayable",
         "type": "function"
+    };
+
+    const burnABI = {
+        "inputs": [
+            {
+                "internalType": "uint256",
+                "name": "tokenId",
+                "type": "uint256"
+            }
+        ],
+        "name": "burn",
+        "outputs": [],
+        "stateMutability": "nonpayable",
+        "type": "function"
+    };
+
+    const addTotalSupplyABI = {
+        "inputs": [
+            {
+                "internalType": "uint256",
+                "name": "amount_",
+                "type": "uint256"
+            }
+        ],
+        "name": "addTotalSupply",
+        "outputs": [],
+        "stateMutability": "nonpayable",
+        "type": "function"
+    };
+
+    const pauseABI = {
+        "inputs": [],
+        "name": "pause",
+        "outputs": [],
+        "stateMutability": "nonpayable",
+        "type": "function"
+    };
+
+    const unpauseABI = {
+        "inputs": [],
+        "name": "unpause",
+        "outputs": [],
+        "stateMutability": "nonpayable",
+        "type": "function"
+    };
+
+    const setBaseURIABI = {
+      "inputs": [
+        {
+          "internalType": "string",
+          "name": "baseURI_",
+          "type": "string"
+        }
+      ],
+      "name": "setBaseURI",
+      "outputs": [],
+      "stateMutability": "nonpayable",
+      "type": "function"
+    };
+
+    const transferOwnershipABI = {
+      "inputs": [
+        {
+          "internalType": "address",
+          "name": "newOwner",
+          "type": "address"
+        }
+      ],
+      "name": "transferOwnership",
+      "outputs": [],
+      "stateMutability": "nonpayable",
+      "type": "function"
     };
 
     beforeEach(async function () {
@@ -131,10 +204,30 @@ contract('OurSongNFToken', function (accounts) {
 
             expect(await this.nfToken.balanceOf(this.sstAdmin.address)).to.be.bignumber.equal(new BN(0));
         });
+
+        it('can transfer admin and can not mint invalid token id', async function () {
+            let transferOwnershipCallData = web3.eth.abi.encodeFunctionCall(transferOwnershipABI, [initialHolder]);
+            let receipt = await this.sstAdmin.execute(this.nfToken.address, transferOwnershipCallData, 1, { from: initialHolder });
+            expectEvent(receipt, 'Execution');
+
+            await this.nfToken.mint(initialHolder, 1, { from: initialHolder });
+
+            expect(await this.nfToken.ownerOf(1)).to.equal(initialHolder);
+
+            await expectRevert(
+                this.nfToken.mint(initialHolder, 0, { from: initialHolder }),
+                'Mint: token id must not be zero'
+            );
+
+            await expectRevert(
+                this.nfToken.mint(initialHolder, 6, { from: initialHolder }),
+                'Mint: token id must not bigger than total supply'
+            );
+        });
     });
 
     describe('should have right balance after transfer', async function () {
-        it('can transfer by owner', async function () {
+        it('can transfer by token owner', async function () {
             let mintCallData = web3.eth.abi.encodeFunctionCall(mintABI, [this.sstAdmin.address, 1]);
             let receipt = await this.sstAdmin.execute(this.nfToken.address, mintCallData, 1, { from: initialHolder });
             expectEvent(receipt, 'Execution');
@@ -163,6 +256,84 @@ contract('OurSongNFToken', function (accounts) {
             await this.nfToken.transferFrom(recipient, initialHolder, 1, { from: recipient });
 
             expect(await this.nfToken.ownerOf(1)).to.equal(initialHolder);
+        });
+    });
+
+    describe('should have burn function', async function () {
+        it('can burn by token owner', async function () {
+            let mintCallData = web3.eth.abi.encodeFunctionCall(mintABI, [this.sstAdmin.address, 1]);
+            let receipt = await this.sstAdmin.execute(this.nfToken.address, mintCallData, 1, { from: initialHolder });
+            expectEvent(receipt, 'Execution');
+
+            expect(await this.nfToken.ownerOf(1)).to.equal(this.sstAdmin.address);
+
+            mintCallData = web3.eth.abi.encodeFunctionCall(mintABI, [this.sstAdmin.address, 2]);
+            receipt = await this.sstAdmin.execute(this.nfToken.address, mintCallData, 2, { from: anotherAccount });
+            expectEvent(receipt, 'Execution');
+
+            expect(await this.nfToken.ownerOf(2)).to.equal(this.sstAdmin.address);
+
+            let burnCallData = web3.eth.abi.encodeFunctionCall(burnABI, [1]);
+            receipt = await this.sstAdmin.execute(this.nfToken.address, burnCallData, 3, { from: initialHolder });
+            expectEvent(receipt, 'Execution');
+
+            await expectRevert(
+                this.nfToken.ownerOf(1),
+                'ERC721: owner query for nonexistent token'
+            );
+        });
+    });
+
+    describe('should have add total supply function', async function () {
+        it('can add total supply by admin', async function () {
+            expect(await this.nfToken.totalSupply()).to.be.bignumber.equal(INITIAL_SUPPLY);
+
+            let addTotalSupplyCallData = web3.eth.abi.encodeFunctionCall(addTotalSupplyABI, [ADD_SUPPLY]);
+            let receipt = await this.sstAdmin.execute(this.nfToken.address, addTotalSupplyCallData, 1, { from: initialHolder });
+            expectEvent(receipt, 'Execution');
+
+            expect(await this.nfToken.totalSupply()).to.be.bignumber.equal(INITIAL_SUPPLY.add(ADD_SUPPLY));
+        });
+    });
+
+    describe('should have pause/unpause function', async function () {
+        it('can pause/unpause by admin', async function () {
+            expect(await this.nfToken.paused()).equal(false);
+
+            let pauseCallData = web3.eth.abi.encodeFunctionCall(pauseABI, []);
+            let receipt = await this.sstAdmin.execute(this.nfToken.address, pauseCallData, 1, { from: initialHolder });
+            expectEvent(receipt, 'Execution');
+
+            expect(await this.nfToken.paused()).equal(true);
+
+            let unpauseCallData = web3.eth.abi.encodeFunctionCall(unpauseABI, []);
+            receipt = await this.sstAdmin.execute(this.nfToken.address, unpauseCallData, 1, { from: initialHolder });
+            expectEvent(receipt, 'Execution');
+
+            expect(await this.nfToken.paused()).equal(false);
+        });
+    });
+
+    describe('should have set base URI function', async function () {
+        it('can set base URI by admin', async function () {
+            expect(await this.nfToken.baseURI()).to.be.bignumber.equal(INITIAL_BASE_URI);
+
+            let setBaseURICallData = web3.eth.abi.encodeFunctionCall(setBaseURIABI, [NEW_BASE_URI]);
+            let receipt = await this.sstAdmin.execute(this.nfToken.address, setBaseURICallData, 1, { from: initialHolder });
+            expectEvent(receipt, 'Execution');
+
+            expect(await this.nfToken.baseURI()).to.be.bignumber.equal(NEW_BASE_URI);
+        });
+
+        it('can not set base URI by not admin', async function () {
+            expect(await this.nfToken.baseURI()).to.be.bignumber.equal(INITIAL_BASE_URI);
+
+            await expectRevert(
+                this.nfToken.setBaseURI(NEW_BASE_URI, { from: initialHolder }),
+                'Ownable: caller is not the owner'
+            );
+
+            expect(await this.nfToken.baseURI()).to.be.bignumber.equal(INITIAL_BASE_URI);
         });
     });
 });
